48bdf823884750c0ce5a974db9468a95ba4b4944
aqurilla <aqurilla@gmail.com>
2020-01-30 16:04:27 -0800

    compare distribution strategies

--- Cloud Data Warehouses/practice/aws_iac.py
+++ Cloud Data Warehouses/practice/aws_iac.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-# Setting up IAM and Redshift cluster
+# Configuring Redshift cluster
 """
 
 import pandas as pd
@@ -8,6 +8,9 @@ import boto3
 import json
 import configparser
 import psycopg2
+from time import time
+import matplotlib.pyplot as plt
+import pandas as pd
 
 config = configparser.ConfigParser()
 config.read_file(open('dwh.cfg'))
@@ -153,6 +156,222 @@ print(conn_string)
 
 # Commented out IPython magic to ensure Python compatibility.
 # %sql $conn_string
+
+# Commented out IPython magic to ensure Python compatibility.
+# # Compare distribution strategies
+# # Setup 4 dim tables and 1 fact table (lineorder)
+# # https://docs.aws.amazon.com/redshift/latest/dg/t_Distributing_data.html
+# 
+# %%sql
+# CREATE SCHEMA IF NOT EXISTS nodist;
+# SET search_path TO nodist;
+# 
+# DROP TABLE IF EXISTS part;
+# DROP TABLE IF EXISTS supplier;
+# DROP TABLE IF EXISTS customer;
+# DROP TABLE IF EXISTS dwdate;
+# DROP TABLE IF EXISTS lineorder;
+# 
+# CREATE TABLE part 
+# (
+#   p_partkey     INTEGER NOT NULL,
+#   p_name        VARCHAR(22) NOT NULL,
+#   p_mfgr        VARCHAR(6) NOT NULL,
+#   p_category    VARCHAR(7) NOT NULL,
+#   p_brand1      VARCHAR(9) NOT NULL,
+#   p_color       VARCHAR(11) NOT NULL,
+#   p_type        VARCHAR(25) NOT NULL,
+#   p_size        INTEGER NOT NULL,
+#   p_container   VARCHAR(10) NOT NULL
+# );
+# 
+# CREATE TABLE supplier 
+# (
+#   s_suppkey   INTEGER NOT NULL,
+#   s_name      VARCHAR(25) NOT NULL,
+#   s_address   VARCHAR(25) NOT NULL,
+#   s_city      VARCHAR(10) NOT NULL,
+#   s_nation    VARCHAR(15) NOT NULL,
+#   s_region    VARCHAR(12) NOT NULL,
+#   s_phone     VARCHAR(15) NOT NULL
+# );
+# 
+# CREATE TABLE customer 
+# (
+#   c_custkey      INTEGER NOT NULL,
+#   c_name         VARCHAR(25) NOT NULL,
+#   c_address      VARCHAR(25) NOT NULL,
+#   c_city         VARCHAR(10) NOT NULL,
+#   c_nation       VARCHAR(15) NOT NULL,
+#   c_region       VARCHAR(12) NOT NULL,
+#   c_phone        VARCHAR(15) NOT NULL,
+#   c_mktsegment   VARCHAR(10) NOT NULL
+# );
+# 
+# CREATE TABLE dwdate 
+# (
+#   d_datekey            INTEGER NOT NULL,
+#   d_date               VARCHAR(19) NOT NULL,
+#   d_dayofweek          VARCHAR(10) NOT NULL,
+#   d_month              VARCHAR(10) NOT NULL,
+#   d_year               INTEGER NOT NULL,
+#   d_yearmonthnum       INTEGER NOT NULL,
+#   d_yearmonth          VARCHAR(8) NOT NULL,
+#   d_daynuminweek       INTEGER NOT NULL,
+#   d_daynuminmonth      INTEGER NOT NULL,
+#   d_daynuminyear       INTEGER NOT NULL,
+#   d_monthnuminyear     INTEGER NOT NULL,
+#   d_weeknuminyear      INTEGER NOT NULL,
+#   d_sellingseason      VARCHAR(13) NOT NULL,
+#   d_lastdayinweekfl    VARCHAR(1) NOT NULL,
+#   d_lastdayinmonthfl   VARCHAR(1) NOT NULL,
+#   d_holidayfl          VARCHAR(1) NOT NULL,
+#   d_weekdayfl          VARCHAR(1) NOT NULL
+# );
+# 
+# CREATE TABLE lineorder 
+# (
+#   lo_orderkey          INTEGER NOT NULL,
+#   lo_linenumber        INTEGER NOT NULL,
+#   lo_custkey           INTEGER NOT NULL,
+#   lo_partkey           INTEGER NOT NULL,
+#   lo_suppkey           INTEGER NOT NULL,
+#   lo_orderdate         INTEGER NOT NULL,
+#   lo_orderpriority     VARCHAR(15) NOT NULL,
+#   lo_shippriority      VARCHAR(1) NOT NULL,
+#   lo_quantity          INTEGER NOT NULL,
+#   lo_extendedprice     INTEGER NOT NULL,
+#   lo_ordertotalprice   INTEGER NOT NULL,
+#   lo_discount          INTEGER NOT NULL,
+#   lo_revenue           INTEGER NOT NULL,
+#   lo_supplycost        INTEGER NOT NULL,
+#   lo_tax               INTEGER NOT NULL,
+#   lo_commitdate        INTEGER NOT NULL,
+#   lo_shipmode          VARCHAR(10) NOT NULL
+# );
+
+# Commented out IPython magic to ensure Python compatibility.
+# # Same setup in different schema using KEY distribution
+# # strategies
+# 
+# %%sql
+# 
+# CREATE SCHEMA IF NOT EXISTS dist;
+# SET search_path TO dist;
+# 
+# DROP TABLE IF EXISTS part;
+# DROP TABLE IF EXISTS supplier;
+# DROP TABLE IF EXISTS customer;
+# DROP TABLE IF EXISTS dwdate;
+# DROP TABLE IF EXISTS lineorder;
+# 
+# CREATE TABLE part 
+# (
+#   p_partkey     INTEGER NOT NULL sortkey distkey,
+#   p_name        VARCHAR(22) NOT NULL,
+#   p_mfgr        VARCHAR(6) NOT NULL,
+#   p_category    VARCHAR(7) NOT NULL,
+#   p_brand1      VARCHAR(9) NOT NULL,
+#   p_color       VARCHAR(11) NOT NULL,
+#   p_type        VARCHAR(25) NOT NULL,
+#   p_size        INTEGER NOT NULL,
+#   p_container   VARCHAR(10) NOT NULL
+# );
+# 
+# CREATE TABLE supplier 
+# (
+#   s_suppkey   INTEGER NOT NULL sortkey,
+#   s_name      VARCHAR(25) NOT NULL,
+#   s_address   VARCHAR(25) NOT NULL,
+#   s_city      VARCHAR(10) NOT NULL,
+#   s_nation    VARCHAR(15) NOT NULL,
+#   s_region    VARCHAR(12) NOT NULL,
+#   s_phone     VARCHAR(15) NOT NULL
+# );
+# 
+# CREATE TABLE customer 
+# (
+#   c_custkey      INTEGER NOT NULL sortkey,
+#   c_name         VARCHAR(25) NOT NULL,
+#   c_address      VARCHAR(25) NOT NULL,
+#   c_city         VARCHAR(10) NOT NULL,
+#   c_nation       VARCHAR(15) NOT NULL,
+#   c_region       VARCHAR(12) NOT NULL,
+#   c_phone        VARCHAR(15) NOT NULL,
+#   c_mktsegment   VARCHAR(10) NOT NULL
+# );
+# 
+# CREATE TABLE dwdate 
+# (
+#   d_datekey            INTEGER NOT NULL sortkey,
+#   d_date               VARCHAR(19) NOT NULL,
+#   d_dayofweek          VARCHAR(10) NOT NULL,
+#   d_month              VARCHAR(10) NOT NULL,
+#   d_year               INTEGER NOT NULL,
+#   d_yearmonthnum       INTEGER NOT NULL,
+#   d_yearmonth          VARCHAR(8) NOT NULL,
+#   d_daynuminweek       INTEGER NOT NULL,
+#   d_daynuminmonth      INTEGER NOT NULL,
+#   d_daynuminyear       INTEGER NOT NULL,
+#   d_monthnuminyear     INTEGER NOT NULL,
+#   d_weeknuminyear      INTEGER NOT NULL,
+#   d_sellingseason      VARCHAR(13) NOT NULL,
+#   d_lastdayinweekfl    VARCHAR(1) NOT NULL,
+#   d_lastdayinmonthfl   VARCHAR(1) NOT NULL,
+#   d_holidayfl          VARCHAR(1) NOT NULL,
+#   d_weekdayfl          VARCHAR(1) NOT NULL
+# );
+# 
+# CREATE TABLE lineorder 
+# (
+#   lo_orderkey          INTEGER NOT NULL,
+#   lo_linenumber        INTEGER NOT NULL,
+#   lo_custkey           INTEGER NOT NULL,
+#   lo_partkey           INTEGER NOT NULL distkey,
+#   lo_suppkey           INTEGER NOT NULL,
+#   lo_orderdate         INTEGER NOT NULL sortkey,
+#   lo_orderpriority     VARCHAR(15) NOT NULL,
+#   lo_shippriority      VARCHAR(1) NOT NULL,
+#   lo_quantity          INTEGER NOT NULL,
+#   lo_extendedprice     INTEGER NOT NULL,
+#   lo_ordertotalprice   INTEGER NOT NULL,
+#   lo_discount          INTEGER NOT NULL,
+#   lo_revenue           INTEGER NOT NULL,
+#   lo_supplycost        INTEGER NOT NULL,
+#   lo_tax               INTEGER NOT NULL,
+#   lo_commitdate        INTEGER NOT NULL,
+#   lo_shipmode          VARCHAR(10) NOT NULL
+# );
+
+# Commented out IPython magic to ensure Python compatibility.
+def loadTables(schema, tables):
+    loadTimes = []
+    SQL_SET_SCHEMA = "SET search_path TO {};".format(schema)
+#     %sql $SQL_SET_SCHEMA
+    
+    for table in tables:
+        SQL_COPY= """
+                  COPY {} FROM 's3://awssampledbuswest2/ssbgz/{}' 
+                  CREDENTIALS 'aws_iam_role={}'
+                  gzip REGION 'us-west-2';
+                  """.format(table, table, DWH_ROLE_ARN)
+
+        print("======= LOADING TABLE: ** {} ** IN SCHEMA ==> {} =======".format(table, schema))
+        print(SQL_COPY)
+
+        t0 = time()
+#         %sql $SQL_COPY
+        loadTime = time()-t0
+        loadTimes.append(loadTime)
+
+        print("=== DONE IN: {0:.2f} sec\n".format(loadTime))
+    return pd.DataFrame({"table":tables, "loadtime_"+schema:loadTimes}).set_index('table')
+
+tables = ["customer","dwdate","supplier", "part", "lineorder"]
+
+# Insertion into each schema
+nodistStats = loadTables("nodist", tables)
+distStats = loadTables("dist", tables)
 
 # Delete cluster
 redshift.delete_cluster(ClusterIdentifier=DWH_CLUSTER_IDENTIFIER, SkipFinalClusterSnapshot=True)
